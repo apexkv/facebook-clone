a15c77d9ee1cf21ecdd16a4eda6f753fcdfb428d
Kavindu Harshitha <kavindu@apexkv.com>
2024-09-22 08:54:14 +0930

    Add Swagger documentation for FB-Clone API

--- friends/friends/urls.py
+++ friends/friends/urls.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
 from django.urls import path
 from friendship import views
+from rest_framework import permissions
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="FB-Clone API(Friends)",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-redoc"),
     path("admin/", admin.site.urls),
     # Sent friend requests
     path("requests/sent/", views.FriendRequestView.as_view({"get": "list_sent"})),
