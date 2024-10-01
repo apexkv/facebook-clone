206749e734b162fd3244e7b3ab87a12c88206fbd
Kavindu Harshitha <kavindu@apexkv.com>
2024-10-01 16:40:59 +0930

    RESET THE PROJECT

--- friends/friendship/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import FriendRequest, User
-
-
-class FriendRequestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FriendRequest
-        fields = [""]
