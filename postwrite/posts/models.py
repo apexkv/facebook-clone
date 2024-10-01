a792ddee1fba7aaf6375a97e7a16e67172a52ec0
Kavindu Harshitha <kavindu@apexkv.com>
2024-10-01 20:26:07 +0930

    created User model

--- postwrite/posts/models.py
+++ postwrite/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False)
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
