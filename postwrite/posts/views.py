566666d31716e9e103354ef7c4c4bfab1ad874d9
Kavindu Harshitha <kavindu@apexkv.com>
2024-10-01 21:14:25 +0930

    created view for post creation

--- postwrite/posts/views.py
+++ postwrite/posts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import PostSerializer
+
+
+class PostViewSet(ModelViewSet):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_context(self):
+        context = super(PostViewSet, self).get_serializer_context()
+        context.update({"request": self.request})
+        return context
