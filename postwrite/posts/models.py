f033e04133de37f72951fdd333cc5b7817a5c7e1
Kavindu Harshitha <kavindu@apexkv.com>
2024-10-01 21:13:36 +0930

    updated User model

--- postwrite/posts/models.py
+++ postwrite/posts/models.py
@@ -4,12 +4,9 @@ from django.db import models
 
 class User(models.Model):
     id = models.UUIDField(primary_key=True, editable=False)
-    full_name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.email
+        return self.id
 
 
 class Post(models.Model):
