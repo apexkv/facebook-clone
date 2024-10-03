efd91d7010644d6fd04d74134dcd0adfcbd2eec8
Kavindu Harshitha <kavindu@apexkv.com>
2024-10-03 13:30:35 +0930

    created authentication middleware

---
+++ friendship/friends/middleware.py
@@ -0,0 +1,37 @@
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+from django.conf import settings
+import requests
+
+from .models import User
+
+
+class UserAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        token = request.headers.get("Authorization")
+
+        if not token:
+            return None
+
+        auth_service_url = settings.USERS_SERVICE + "/api/users/me/"
+
+        try:
+            response = requests.get(auth_service_url, headers={"Authorization": token})
+            if response.status_code == 200:
+                user_data = response.json()
+                return (
+                    self.get_user(user_data["id"]),
+                    token.split("JWT")[-1],
+                )
+            else:
+                raise AuthenticationFailed("Invalid token")
+        except requests.exceptions.RequestException:
+            raise AuthenticationFailed("Auth service unavailable")
+
+    def get_user(self, user_data):
+        """
+        Retrieve or create a Neo4j user instance based on the user data from the external service.
+        """
+        user = User.nodes.get_or_none(user_id=user_data["id"])
+        user.is_authenticated = True
+        return user
