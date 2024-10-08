054ba51ede14cd3ae1a900e62d5f96cede38b2bd
Kavindu Harshitha <kavindu@apexkv.com>
2024-10-08 21:04:21 +1030

    Refactor User model to include email and full name fields

--- postwrite/posts/models.py
+++ postwrite/posts/models.py
@@ -4,9 +4,11 @@ from django.db import models
 
 class User(models.Model):
     id = models.UUIDField(primary_key=True, editable=False)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=500)
 
     def __str__(self):
-        return self.id
+        return self.email
 
 
 class Post(models.Model):
