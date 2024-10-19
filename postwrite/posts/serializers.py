0aa2c78b01a7a5b6fbada78796226cec5f735cb3
Kavindu Harshitha <kavindu@apexkv.com>
2024-10-19 18:36:31 +1030

    updated post serializer

--- postwrite/posts/serializers.py
+++ postwrite/posts/serializers.py
@@ -3,7 +3,17 @@ from rest_framework import serializers
 from .models import Post, User
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
 class PostSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+    user = UserSerializer(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+
     class Meta:
         model = Post
         fields = ["content"]
@@ -12,9 +22,3 @@ class PostSerializer(serializers.ModelSerializer):
         user = self.context["request"].user
         post = Post.objects.create(user=user, **validated_data)
         return post
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = "__all__"
