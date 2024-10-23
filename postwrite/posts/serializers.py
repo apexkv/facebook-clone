7b8e970ba87cf869bd79ebe796b394a81e6ae63e
Kavindu Harshitha <kavindu@apexkv.com>
2024-10-23 20:42:56 +1030

    created comment serializer

--- postwrite/posts/serializers.py
+++ postwrite/posts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Post, User
+from .models import Post, User, Comment, PostLike, CommentLike
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,12 +13,30 @@ class PostSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
     user = UserSerializer(read_only=True)
     created_at = serializers.DateTimeField(read_only=True)
+    like_count = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Post
-        fields = ["id", "user", "created_at", "content"]
+        fields = ["id", "user", "created_at", "content", "like_count"]
 
     def create(self, validated_data):
         user = self.context["request"].user
         post = Post.objects.create(user=user, **validated_data)
         return post
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+    user = UserSerializer(read_only=True)
+    post = PostSerializer(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    like_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ["id", "user", "post", "created_at", "content", "like_count"]
+
+    def create(self, validated_data):
+        user = self.context["request"].user
+        comment = Comment.objects.create(user=user, **validated_data)
+        return comment
